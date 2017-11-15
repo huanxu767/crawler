c1a20679392914b4bb4fc0cfca48e3b26db019d3
许先生 <huanxu767@qq.com>
2017-11-15 17:36:20 +0800

    压力测试

--- src/main/java/com/hb/crawler/controller/TestController.java
+++ src/main/java/com/hb/crawler/controller/TestController.java
@@ -21,15 +21,45 @@ public class TestController {
 
     static Logger logger = LoggerFactory.getLogger(TestController.class);
 
-    @Autowired
-    private RedisUtils redisUtils;
+    @RequestMapping(value = "/ddd")
+    public String ddd(int x) {
+        Map map = new HashMap<>();
+        int i = 0 ;
+        while(i < x){
+            i++;
+            System.out.println(i);
+            final WebClient wc = new WebClient(BrowserVersion.CHROME);
+            wc.setJavaScriptTimeout(10000);
+            wc.getOptions().setJavaScriptEnabled(true); // 启用JS解释器，默认为true
+            wc.getOptions().setCssEnabled(false); // 禁用css支持
+            wc.getOptions().setThrowExceptionOnScriptError(false); // js运行错误时，是否抛出异常
+            wc.getOptions().setTimeout(10000); // 设置连接超时时间 ，这里是10S。如果为0，则无限期等待
+            wc.waitForBackgroundJavaScript(10000);
+            try {
+                HtmlPage loginPage = wc.getPage(JsChinaMobileUrl.LOGIN_URL);
+                System.out.println(loginPage.getTitleText());
+//                String js = "$('#userNumber').val('13585119230');" +
+//                        "$('#userPassword').val('456123');" +
+//                        "$('#popBox-login-button').click();";
+//                ScriptResult scriptResult = loginPage.executeJavaScript(js);
+//                final HtmlPage homePage = (HtmlPage) scriptResult.getNewPage();
+//                if(!homePage.getTitleText().contains("登录")){
+//                    System.out.println("成功");
+//                }else{
+//                    System.out.println("失败");
+//                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+//                wc.close();
+            }
+            map.put(i,wc);
+        }
+        return "OK";
+    }
 
 
-    /**
-     * 查询通话记录
-     *
-     * @return
-     */
+
     @RequestMapping(value = "/getImg")
     public String test2() {
         Map map = new HashMap<>();
