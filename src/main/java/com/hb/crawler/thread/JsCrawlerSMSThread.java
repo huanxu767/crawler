cc2309503eac24a02bc68fa445ee932c5ce13c61
许先生 <huanxu767@qq.com>
2017-12-04 17:07:05 +0800

    短信发送 尝试两次

--- src/main/java/com/hb/crawler/thread/JsCrawlerSMSThread.java
+++ src/main/java/com/hb/crawler/thread/JsCrawlerSMSThread.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+
 public class JsCrawlerSMSThread implements Runnable {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -21,26 +22,40 @@ public class JsCrawlerSMSThread implements Runnable {
     @Override
     public void run() {
         JsBrowserInstance jsBrowserInstance = JsBrowserCache.get(instanceId);
-        smsAuthorization(jsBrowserInstance.getWebClient());
+        boolean flag = false;
+        //第一次触发发送短信
+        try {
+            smsAuthorization(jsBrowserInstance.getWebClient());
+        } catch (Exception e) {
+            logger.error("第一次触发短信失败:"+instanceId,e);
+            flag = true;
+        }
+        //第一次触发失败 触发第二次
+        if(flag){
+            try {
+                smsAuthorization(jsBrowserInstance.getWebClient());
+            } catch (Exception e) {
+                logger.error("第二次触发短信失败:"+instanceId,e);
+
+            }
+        }
         jsBrowserInstance.setHasSendMsg(true);
     }
 
     /**
      * 发送短信
      */
-    void smsAuthorization(WebClient webClient) {
+    void smsAuthorization(WebClient webClient) throws Exception {
         webClient.getOptions().setJavaScriptEnabled(true);
         webClient.getOptions().setRedirectEnabled(true);
+        sendMsg(webClient);
+    }
+    void sendMsg(WebClient webClient) throws Exception {
         String js = "$('#txtToDate').next('a').click();";
-        HtmlPage page;
-        try {
-            page = webClient.getPage(JsChinaMobileUrl.MY_DETAIL_CALL_URL);
-            page.executeJavaScript(js);
-            synchronized (page) {
-                page.wait(2000);
-            }
-        } catch (Exception e) {
-            logger.error("发送短信验证码", e);
+        HtmlPage page = webClient.getPage(JsChinaMobileUrl.MY_DETAIL_CALL_URL);
+        page.executeJavaScript(js);
+        synchronized (page) {
+            page.wait(2000);
         }
     }
 }
